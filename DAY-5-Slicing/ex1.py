8e32c2ce13b4d70b29d76303620db1e27d800d38
mani015 <ramanadhulamanikumar01975@gmail.com>
2023-03-17 16:04:26 +0530

    'py'

---
+++ DAY-5-Slicing/ex1.py
@@ -0,0 +1,24 @@
+
+# Posotive slicing
+
+# sy: objectname[startingvalue : endingvalue(-1)]
+
+x = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+print(x)
+# To calling with out ending value
+# print(x[0:])
+# [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+
+
+# TO using ending value
+
+# print(x[0:5])
+# [1, 2, 3, 4, 5]
+
+
+print(x.pop())
+print(x)
+
+# [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+# 17
+# [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
---
+++ DAY-5-Slicing/ex4.py
@@ -0,0 +1,38 @@
+
+x =['a','b','c','a','e','r','t','g','b','s','x','c','r','t','m','z','s','q','w','i','o','p']
+
+print(x.index('a'))
+# 0
+
+print(x.index('a',1,10))
+# 3
+
+print(x.index('b'))
+# 1
+
+print(x.index('b',2,9))
+# 8
+
+print(x.index('c'))
+# 2
+
+print(x.index('c',3,12))
+# 11
+
+print(x.index('e'))
+# 4
+print(x.index('r'))
+# 5
+print(x.index('r',6,13))
+# 12
+
+print(x.index('t'))
+# 6
+print(x.index('t',7,15))
+# 13
+print(x.index('s',))
+# 9
+
+print(x.index('s',10,17))
+# 16
+
