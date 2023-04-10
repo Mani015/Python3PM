3789ab0bb5218bea11b52849f2e7cbf31c14ec32
GRK <realtimeprojectgrk1@gmail.com>
2023-04-10 15:55:04 +0530

    'py'

---
+++ Day-14-UserInput/ex2.py
@@ -0,0 +1,13 @@
+# Mention datatype:
+
+x = int(input('Enter 1st number:'))
+
+print(type(x))
+
+y = int(input('Enter 2nd nuber:'))
+print(type(y))
+
+
+print('addtion of :',x + y)
+
+
---
+++ Day-14-UserInput/ex5.py
@@ -0,0 +1,11 @@
+x = complex(input('Enter 1st number:'))
+
+print(type(x))
+
+y = complex(input('Enter 2nd nuber:'))
+print(type(y))
+
+print('addtion of x,y:',x + y)
+print('subtraction of x,y:',x - y)
+print('Multiplication of x,y:',x * y)
+print('Division of x,y:',x / y)
