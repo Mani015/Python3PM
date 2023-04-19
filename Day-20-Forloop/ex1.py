bdaf56ba652b96617573158c6c842182e0f3b3c8
GRK <realtimeprojectgrk1@gmail.com>
2023-04-19 16:13:53 +0530

    'py'

---
+++ Day-20-Forloop/ex1.py
@@ -0,0 +1,23 @@
+
+# iterable : string, list tuple,set
+
+# for value in iterable:
+#         statement---1
+#          statement---2
+
+
+
+x = [1,2,3,4,5,6,7,8]
+print(x)
+# [1, 2, 3, 4, 5, 6, 7, 8]
+
+for i in x:
+    print(i)
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
---
+++ Day-20-Forloop/ex4.py
@@ -0,0 +1,38 @@
+# excute even numbers in given list
+
+a = [1,2,34,5,6,7,8,9,10,11,22,33,44,55,66,12,13,14,15,16]
+
+for i in a:
+    if i%2==0:
+        print('even number:',i)
+# even number: 2
+# even number: 34
+# even number: 6
+# even number: 8
+# even number: 10
+# even number: 22
+# even number: 44
+# even number: 66
+# even number: 12
+# even number: 14
+# even number: 16
+
+
+# using for loop
+
+for k in range(20):
+    if k%2==0:
+        print(k)
+# 0
+# 2
+# 4
+# 6
+# 8
+# 10
+# 12
+# 14
+# 16
+# 18
+
+
+
