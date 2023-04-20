1d81cbc6a02a3889566aac1177d25b6c0231acfe
GRK <realtimeprojectgrk1@gmail.com>
2023-04-20 16:15:33 +0530

    'py'

---
+++ Day-21-For loop examples/ex1.py
@@ -0,0 +1,22 @@
+x = [1,2,3,4,5,6,7,8,9,10]
+
+# print(x)
+
+# for objectname in iterable
+
+# for i in x:
+#     print(i)
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# range(starting value, endingvalue,stepover value)
+for i in range(0,50,2):
+    print(i*2)
+# 0-->0+2=2>>>2+2=4-->4+2=6
---
+++ Day-21-For loop examples/ex2.py
@@ -0,0 +1,27 @@
+
+# for i in range(1,21):
+#     print('hello python',i)
+
+
+
+x = [2,4,8,9,13,15,18,20,24,29,30]
+
+for k in x:
+    if k%2==0:
+        print('even number:',k)
+    else:
+        print('odd number:',k)
+
+# even number: 2
+# even number: 4
+# even number: 8
+# odd number: 9
+# odd number: 13
+# odd number: 15
+# even number: 18
+# even number: 20
+# even number: 24
+# odd number: 29
+# even number: 30
+
+
