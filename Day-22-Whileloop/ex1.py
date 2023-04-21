71c0cbaa7bfe3d0ac1c4d9ebb8e20b53609f8d2d
GRK <realtimeprojectgrk1@gmail.com>
2023-04-21 22:28:11 +0530

    'py'

---
+++ Day-22-Whileloop/ex1.py
@@ -0,0 +1,46 @@
+# while loop:In Python,
+# The while loop statement repeatedly executes a code block while a particular condition is true
+
+# In simple words, The while loop enables the Python program to repeat a set of operations while a particular condition
+# is true. When the condition becomes false, execution comes out of the loop immediately,
+# and the first statement after the while loop is executed.
+
+
+i=0
+
+# while condition:
+#     print(lines of statements)
+
+
+# while i<2:
+#     print(i)
+#     i=i+1
+#
+# print('loop excuted')
+
+# 0
+# 1
+# loop excuted
+
+
+
+k = 1
+
+while k<10:
+    print('loop excute:',k)
+    k= k + 1
+print('come out the loop while condition become as false')
+
+# loop excute: 1
+# loop excute: 2
+# loop excute: 3
+# loop excute: 4
+# loop excute: 5
+# loop excute: 6
+# loop excute: 7
+# loop excute: 8
+# loop excute: 9
+
+# come out the loop while condition become as false
+
+
---
+++ Day-22-Whileloop/ex2.py
@@ -0,0 +1,25 @@
+a = 0
+i = 1
+
+while a<11:
+    if i%2==0:
+        print('even number:',i)
+        a = a + 1
+
+    i = i + 1
+
+print('loop excuted compelte')
+
+
+# even number: 0
+# even number: 2
+# even number: 4
+# even number: 6
+# even number: 8
+# even number: 10
+# even number: 12
+# even number: 14
+# even number: 16
+# even number: 18
+# even number: 20
+# loop excuted compelte
