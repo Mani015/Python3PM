a43618234840db147a0c5d518b1e62edad042f2a
mani015 <ramanadhulamanikumar01975@gmail.com>
2023-05-29 16:08:39 +0530

    'py'

---
+++ Day-39-Compharison_Operator/ex4.py
@@ -0,0 +1,16 @@
+# from collections import Counter
+
+
+x = 'jbvfbvk j k jvkfbvkuvbkvbhvkfehvbdukhbfvbefuvbfoivafhuvliudfiuvehiu'
+
+# print(x)
+# print(Counter(x))
+# Counter({'v': 13, 'b': 9, 'f': 8, 'k': 7, 'u': 7, 'h': 5, 'i': 4, 'j': 3, ' ': 3, 'e': 3, 'd': 2, 'o': 1, 'a': 1, 'l': 1})
+
+
+
+
+import collections as phani
+print(phani.Counter(x))
+# Counter({'v': 13, 'b': 9, 'f': 8, 'k': 7, 'u': 7, 'h': 5, 'i': 4, 'j': 3, ' ': 3, 'e': 3, 'd': 2, 'o': 1, 'a': 1, 'l': 1})
+
