d15903beff2702e4a5c2c152e5e2654e03e1fc2d
mani015 <ramanadhulamanikumar01975@gmail.com>
2023-06-22 10:16:44 +0530

    'py'

---
+++ Day_50_Filehandling/ex2.py
@@ -0,0 +1,7 @@
+
+r1 = open('C:\\Users\DELL\Desktop\Python Definations\\File_Handling.txt','r')
+
+r2 = r1.read()
+print(r2)
+
+r1.close()
---
+++ Day_50_Filehandling/ex3.py
@@ -0,0 +1,12 @@
+
+# f1 = open('file1.txt','w')
+# f2 = f1.write('Love your enemies , and do good to them')
+# print(f2)
+# f1.close()
+#39
+
+f3 = open('file1.txt','a')
+f4 = f3.write('\nDo not judge a book by its cover\nThe truth will set you free')
+
+print(f4)
+
