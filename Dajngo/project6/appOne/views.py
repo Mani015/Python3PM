c6f3f0ba4c504b262d4998d8e21ad4e367e1d04f
mani015 <ramanadhulamanikumar01975@gmail.com>
2023-07-14 16:12:49 +0530

    'py'

---
+++ Dajngo/project6/appOne/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from appOne.models import Student
+
+# Create your views here.
+
+def Studnet_Info(request):
+    details = Student.objects.all()
+    return render(request,'info.html',{'student':details})
