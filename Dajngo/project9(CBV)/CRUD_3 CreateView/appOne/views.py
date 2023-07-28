83dfbc8c3a8849115d120304679f08cb1b38f7c4
mani015 <ramanadhulamanikumar01975@gmail.com>
2023-07-28 15:40:20 +0530

    'py'

---
+++ Dajngo/project9(CBV)/CRUD_3 CreateView/appOne/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from appOne.models import Employee
+from django.views.generic import ListView,DetailView,CreateView
+# Create your views here.
+
+class EmployeeListView(ListView):
+    model = Employee
+    # By defualt template name is 'emp_list.html'
+    # By default context_name is 'emp_list'
+
+class EmployeeDetailView(DetailView):
+    model = Employee
+    # By default template name "employee_detail.html"
+        # By default context_name 'employee'
+class EmployeeCreateView(CreateView):
+    model = Employee
+    fields = ('Firstname','Lastname','Email','Salary')
+    # fields = '__all__'
+    # By default template name "employee_form.html"
+        # By default context_name 'employee'
