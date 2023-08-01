d7172e3ce2a0830475db82306d0d88228972b702
mani015 <ramanadhulamanikumar01975@gmail.com>
2023-08-01 15:59:20 +0530

    'cart'

---
+++ Dajngo/project12(Fruits Cart)/app1/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from app1.models import product,Offer
+# Register your models here.
+
+class productAdmin(admin.ModelAdmin):
+    list_display = ('name','price','quantity','img_url')
+
+class productoffer(admin.ModelAdmin):
+    list_display = ('code','discription','discount')
+
+    
+admin.site.register(Offer,productoffer)
+admin.site.register(product,productAdmin)
